ab8db97b49d1279618b98a1054ecf3d79aec2b2e
Ester Girelli <ester_girelli@estudante.sesisenai.org.br>
2024-05-21 20:30:00 -0300

    ok tabelas

--- migrations/create-contract-table.js
+++ migrations/create-contract-table.js
@@ -8,14 +8,20 @@ async function createContractTable() {
 await connection.query(`USE ${databaseConfig.database}`);
 
     await connection.query(`CREATE TABLE IF NOT EXISTS contrato (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        descricao VARCHAR(1000) NOT NULL,
-        data_contrato DATE NOT NULL,
-        valor_total VARCHAR(15) NOT NULL,
-        duracao VARCHAR(45) NOT NULL,
-        assinatura VARCHAR(45) NOT NULL,
-        USUARIO_isUSUARIO INT NOT NULL,
-        ANUNCIO_idANUNCIO INT NOT NULL
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      descricao VARCHAR(1000) NOT NULL,
+      data_contrato DATE NOT NULL,
+      valor_total VARCHAR(15) NOT NULL,
+      duracao VARCHAR(45) NOT NULL,
+      assinatura VARCHAR(45) NOT NULL,
+      USUARIO_idUSUARIO INT NOT NULL,
+      ANUNCIO_idANUNCIO INT NOT NULL,
+      FOREIGN KEY (USUARIO_idUSUARIO) REFERENCES usuario(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+      FOREIGN KEY (ANUNCIO_idANUNCIO) REFERENCES anuncio(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
     )`);
 
     await connection.end();
